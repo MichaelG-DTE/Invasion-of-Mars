dfb3c34c4bd0064d7b2e5ff92ab308a46876eb3a
MichaelG-DTE <21051@rosmini.school.nz>
2026-02-09 18:52:41 +1300

    Finished Basic Camera
    Finished creating the basic camera movement of the player and added a new script to the level to enable easy quitting of the game

--- scripts/Components/mouse_capture.gd
+++ scripts/Components/mouse_capture.gd
@@ -4,7 +4,8 @@ class_name MouseCapture extends Node
 @export_category("Camera Capture Settings") # organises the export varibles into sections to improve code readabilityt
 @export var current_mouse_mode = Input.MOUSE_MODE_CAPTURED # sets mouse mode to stay within the screen
 @export var mouse_visibility = Input.MOUSE_MODE_HIDDEN #sets mouse cursor to invisible
-@export var sensitivity : float = 0.5 # adjust later
+@export var y_sensitivity : float = 0.005 # adjust later
+@export var x_sensitivity : float = 0.002
 
 var capture_mouse : bool # sets the mouse mode as captured or not based on boolean var
 var mouse_input : Vector2 # gets the input from mouse as coordinates
@@ -13,11 +14,11 @@ func _unhandled_input(event: InputEvent) -> void:
 	# this sets the mouse mode based on the input of mouse motion
 	capture_mouse = event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED
 	if capture_mouse:
-		mouse_input.x += -event.screen_relative.x * sensitivity
-		mouse_input.y += -event.screen_relative.y * sensitivity
+		mouse_input.x += -event.screen_relative.x * x_sensitivity
+		mouse_input.y += -event.screen_relative.y * y_sensitivity
 
 func _ready() -> void:
 	Input.mouse_mode = current_mouse_mode
 
-func  _process(delta: float) -> void:
+func  _process(_delta: float) -> void:
 	mouse_input = Vector2.ZERO
